8ff6330d0d467451eb88d7c7cd4f728efd053de7
Ts-Ahmed <ts.ahmed.edu@gmail.com>
2019-10-08 13:26:05 +0200

    Page affichant la liste des regions fonctionnelle. Lien non-fonctionnel.

--- agvoy-app-03/src/Controller/CouetteEtCafeController.php
+++ agvoy-app-03/src/Controller/CouetteEtCafeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Region;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CouetteEtCafeController extends AbstractController
@@ -15,5 +17,40 @@ class CouetteEtCafeController extends AbstractController
         return $this->render('couette_et_cafe/index.html.twig', [
             'controller_name' => 'CouetteEtCafeController',
         ]);
+    }
+
+
+    /**
+     * Lists all regions
+     * @Route("/region/list", name = "region_list")
+     */
+    public function RegionList(){
+        $em = $this->getDoctrine()->getManager();
+
+        $regions = $em->getRepository(Region::class)->findAll();
+
+        dump($regions);
+
+        return $this->render('couette_et_cafe/index.html.twig', array(
+            'regions' => $regions,
+        ));
+    }
+
+    /**
+     * Finds and displays a region.
+     *
+     * @Route("/{id}", name="region_id", methods="GET")
+     * @param $id
+     * @return Response
+     */
+    public function showAction($id): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $region = $em->getRepository(Region::class)->findOneBy(['id' => $id]);
+        $rooms = $region->getRooms();
+        return $this->render('couette_et_cafe/show.html.twig', array(
+            'rooms' => $rooms,
+        ));
     }
 }
